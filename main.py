02eb4edaf42c85fac2dc27537d769e7fe641cbd6
Rithick <rithick.panoor574@gmail.com>
2024-10-29 19:47:55 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
+# from dotenv import load_dotenv
+
+# load_dotenv() 
+
+app = FastAPI()
+
+client_url = os.getenv("CLIENT_URL")
+
+origins = [
+    client_url,
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=  origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+@app.get("/")
+def read_root():
+    return {
+        "message": "server is running"
+    }
